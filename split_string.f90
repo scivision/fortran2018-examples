06c46841f4165ee71ee2182a7f8dc81bbdde5d72
scivision <scivision@users.noreply.github.com>
2016-05-10 14:56:23 -0400

    init

--- split_string.f90
+++ split_string.f90
@@ -1,10 +1,40 @@
-! split a string into 2 either side of a delimiter token
-function split(instr,  delm)
+module splitstring
     implicit none
-    CHARACTER(len=80),intent(in) :: instr,delm
-    INTEGER :: idx
+contains
+! split a string about a delimiter token, return part before delim
+!
+! with regard to length of CHARACTER, it's probably best to pick a length longer than you'll need
+! and trim rather than using assumed size, particularly if interfacing with other languages
+! CHARACTER assumed size seems to work, but is not reliable in diverse enviroments.
+! Your time is more valueable than a few bytes of RAM.
+character(len=80)  function split(instr,  delm)
 
-    idx = scan(instr,delm)
-    split = instr(1:idx-1)
+        character(len=*), intent(in) :: instr
+        character(len=1), intent(in) :: delm
 
-end function split
+        integer  idx
+
+        idx = scan(instr,delm)
+        split = instr(1:idx-1)
+
+    end function split
+
+end module splitstring
+!---------------------------------------
+program demosplit
+
+    use, intrinsic :: iso_fortran_env, only : stdin=>input_unit
+    use splitstring
+
+    character(len=*),parameter :: mystr="hello.txt"
+    character(len=80) stem
+
+    stem = split(mystr,'.')
+    print *, stem
+
+
+
+end program
+
+
+
