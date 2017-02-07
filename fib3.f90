6b51c9ce4ffb73787abc6133086537a3bebb5dfb
scivision <scivision@users.noreply.github.com>
2017-02-07 12:32:41 -0500

    arrayops

--- fib3.f90
+++ fib3.f90
@@ -6,14 +6,10 @@ implicit none
 
       integer i
 
-DO I=1,N
-    IF (I.EQ.1) THEN
-        A(I) = 0.0D0
-    ELSEIF (I.EQ.2) THEN
-        A(I) = 1.0D0
-    ELSE
-        A(I) = A(I-1) + A(I-2)
-    ENDIF
+A(1:2) = [0., 1.]
+
+DO I=3,N
+    A(I) = A(I-1) + A(I-2)
 ENDDO
 
 END Subroutine
