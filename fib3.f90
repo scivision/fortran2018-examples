dc791ac0cbc4455063ab851a214f955436c6a17c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-12 19:28:02 -0400

    fib

--- fib3.f90
+++ fib3.f90
@@ -1,15 +1,31 @@
-Subroutine FIB(A,N)
-implicit none
-!     CALCULATE FIRST N FIBONACCI NUMBERS
-      INTEGER, Intent(in) :: N
-      REAL*8, Intent(out) :: A(N)
+program runfib
 
-      integer i
+    use iso_c_binding, only: dp=>C_DOUBLE
+    implicit none
 
-A(1:2) = [0., 1.]
+    integer, parameter :: N=1500
+    real(dp) :: A(N)
 
-DO I=3,N
-    A(I) = A(I-1) + A(I-2)
-ENDDO
+    call fib(A)
+    
+    print *,A
 
-END Subroutine
+contains
+
+    Pure Subroutine FIB(A)
+    !     CALCULATE FIRST N FIBONACCI NUMBERS
+
+          REAL(dp), Intent(out) :: A(:)
+
+          integer i,n
+          n = size(A)
+
+    A(1:2) = [0., 1.]
+
+    DO I=3,N
+        A(I) = A(I-1) + A(I-2)
+    ENDDO
+
+    END Subroutine Fib
+
+end program runfib
