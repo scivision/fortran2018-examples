e100206c0a21dbcd1b0c881cb155787561b4e74a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-08-15 07:01:16 -0400

    init

---
+++ mpi/coarray_helloworld.f90
@@ -0,0 +1,33 @@
+program helloworld
+!  Each process prints out a "Hello, world!" message with a process ID
+!  Michael Hirsch, Ph.D.
+
+! Compilation:
+! gfortran -fcoarray=lib coarray_helloworld.f90 -lcaf_mpi
+!
+! prereqs:  Ubuntu 17.04 or newer:
+! apt install libcoarrays-dev
+
+use, intrinsic:: iso_fortran_env, only: dp=>real64, int64
+implicit none
+
+integer(int64) :: rate,tic,toc
+real(dp) :: telaps
+
+! Print a message.
+if (this_image() == 1) then
+    call system_clock(tic)
+    print *, 'number of Fortran coarray images:', num_images()
+end if
+
+print *, 'Process ', this_image()
+
+if ( this_image() == 1 ) then
+    call system_clock(toc)
+    call system_clock(count_rate=rate)
+    telaps = (toc - tic) * 1000.0_dp/rate
+    print '(A,ES10.3,A)', 'Elapsed wall clock time', telaps, ' seconds.'
+end if
+
+end program
+
--- mpi/mpi_helloworld.f90
+++ mpi/mpi_helloworld.f90
@@ -1,41 +1,40 @@
-program main
+program helloworld
 !    Each process prints out a "Hello, world!" message with a process ID
 !  Original Author:  John Burkardt
-!  Modified: Michael Hirsch
+!  Modified: Michael Hirsch, Ph.D.
 
-   use mpi
-   use iso_fortran_env, only: dp=>real64
+use mpi
+use, intrinsic:: iso_fortran_env, only: dp=>real64
+implicit none
 
-    implicit none
-
-  integer error, id, p
-  real(dp) wtime
+integer error, i, p
+real(dp) wtime
 
 
 !  Initialize MPI.
-  call MPI_Init ( error )
+call MPI_Init(error)
 
 !  Get the number of processes.
-  call MPI_Comm_size ( MPI_COMM_WORLD, p, error )
+call MPI_Comm_size(MPI_COMM_WORLD, p, error)
 
 !  Get the individual process ID.
-  call MPI_Comm_rank ( MPI_COMM_WORLD, id, error )
+call MPI_Comm_rank(MPI_COMM_WORLD, i, error)
 
 !  Print a message.
-if (id == 0) then
-    wtime = MPI_Wtime ( )
-    print *, 'number of processes: ', p
+if (i == 0) then
+  wtime = MPI_Wtime()
+  print *, 'number of processes: ', p
 end if
 
-print *, 'Process ', id
+print *, 'Process ', i
 
-if ( id == 0 ) then
-    print *,'HELLO_MPI: Normal end of execution'
-    wtime = MPI_Wtime ( ) - wtime
-    print *, 'Elapsed wall clock time = ', wtime, ' seconds.'
+if ( i == 0 ) then
+  print *,'HELLO_MPI: Normal end of execution'
+  wtime = MPI_Wtime() - wtime
+  print *, 'Elapsed wall clock time = ', wtime, ' seconds.'
 end if
 !  Shut down MPI.
-  call MPI_Finalize ( error )
+  call MPI_Finalize(error)
 
 end program
 
