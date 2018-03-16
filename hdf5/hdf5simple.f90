9dc6a14c46c79dfab8a07884da1c111233a9f013
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-16 18:13:57 -0400

    modern fortran simple example

--- hdf5/hdf5simple.f90
+++ hdf5/hdf5simple.f90
@@ -1,49 +1,68 @@
-! This example is for a typical case of read/write to an N-D array of floating point numbers
-PROGRAM science_data
+! This example is for a typical case of read/write to an n-d array of floating point numbers
+! it overwrites existing "filename", since it is the simplest example
+! https://support.hdfgroup.org/HDF5/doc/HL/RM_H5LT.html
+program hdf5simple
 
-USE ISO_C_BINDING
-USE HDF5
-USE H5LT
+use, intrinsic:: iso_c_binding, only: dp=>c_double
+use h5lt, only : hid_t, hsize_t, &
+   h5f_acc_trunc_f, &  ! overwrite existing file
+   h5open_f, h5fcreate_f, h5ltmake_dataset_double_f, h5fclose_f
 
-IMPLICIT NONE
+implicit none
 ! -------- user parameters -----------
-CHARACTER(LEN=*), PARAMETER :: filename = "floatscience.h5" ! File name
-CHARACTER(LEN=*), PARAMETER :: dsname = "my_image"          ! Dataset name
-INTEGER,parameter :: rank=1  ! rank of user array
-integer, parameter :: length=9
-! -------- initialize HDF5 -----------
-INTEGER(HID_T) :: fid        ! File identifier
-INTEGER(HSIZE_T) :: data_dims(rank)  ! size of array
-! -------- general variables ------------------  
-! https://support.hdfgroup.org/HDF5/doc/RM/PredefDTypes.html
-INTEGER, PARAMETER :: sp = SELECTED_REAL_KIND(6,37)   ! single-precision float
-INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15,307) ! double-precision float
-integer(HID_T) :: sptype, dptype
+character(*), parameter :: filename = "hdf5simple.h5" ,& ! file name
+                            dsname = "1toN"          ! dataset name
+real(dp), allocatable :: array(:)
+! -------- HDF5
+integer(hid_t) :: fid
+integer :: error, length
+character(8) :: argv  ! just to keep maximum file size less than about a gigabyte (sanity check)
+!-------------------------------------------------
 
-INTEGER :: error ! Error flag
-integer :: i
+call get_command_argument(1,argv,status=error)
+if(error/=0) error stop 'please input array length'
+read(argv,'(I8)') length
+allocate(array(length))
+
+array = fakedata(length)
+call h5write(filename,array)
+
+contains
+
+function fakedata(length)
+  integer, intent(in) :: length
+  integer :: i
+  real(dp) :: fakedata(length)
+
+  do concurrent (i = 1:length)
+    fakedata(i) = real(i,dp)
+  end do
+
+end function fakedata
 
 
-!----------------------
-! data to write (arbitrary)
-REAL(dp) :: array(length)
-do i = 1,length
-    array(i) = dble(i)
-end do  
+subroutine h5write(filename,array)
+! convenience function I made, for easy resource.
+! it overwrites existing "filename"
 
-data_dims = size(array)
-!---------- Initialize FORTRAN interface
-call h5open_f(error) 
-!--- create file
-CALL H5Fcreate_F(filename, H5F_ACC_TRUNC_F,fid,error)
-if (error /= 0) error stop 'could not open HDF5 library'
-!---------- write array to HDF5
-!call h5ltmake_dataset_double_f(fid,dsname,rank,data_dims,array,error)  
- call h5ltmake_dataset_f(fid,dsname,rank,data_dims, H5T_NATIVE_DOUBLE, array, error)   ! h5kind_to_type(dp,H5_REAL_KIND)
-if (error /= 0) error stop 'could not write data'
-!----------- close file
-call H5Fclose_F(fid,error)
-if (error /= 0) error stop 'could not close file'
+  character(*), intent(in) :: filename
+  real(dp), intent(in) :: array(:)
+  !---------- initialize fortran interface
+  call h5open_f(error)
+  if (error /= 0) error stop 'could not open hdf5 library'
+  !--- create file
+  call h5fcreate_f(filename, h5f_acc_trunc_f,fid,error)
+  if (error /= 0) error stop 'could not create file'
+  !---------- write array to hdf5
+  call h5ltmake_dataset_double_f(fid,dsname,rank(array),shape(array,hsize_t),array,error)
+  if (error /= 0) error stop 'could not write data'
+  !----------- close file
+  call h5fclose_f(fid,error)
+  if (error /= 0) error stop 'could not close file'
 
-END PROGRAM science_data
+  print *,'created ',filename
+
+end subroutine h5write
+
+end program
 
