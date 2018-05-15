6fda6f78a85846fb7ca5ab22e9c84cc150e47c76
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-05-15 12:57:22 -0400

    more complete random_init

--- random_init.f90
+++ random_init.f90
@@ -1,5 +1,7 @@
 program testinit
 
+use, intrinsic:: iso_fortran_env, only: error_unit
+
 implicit none
 
 call init_random_seed()
@@ -11,23 +13,28 @@ contains
 subroutine init_random_seed()
   ! NOTE: this subroutine is replaced by "call random_init()" intrinsic of Fortran 2018
   integer :: n, u,ios
-  integer, allocatable :: seed(:)
+  integer, allocatable :: seed(:),check(:)
 
   call random_seed(size=n)
-  allocate(seed(n))
+  allocate(seed(n),check(n))
   
   open(newunit=u, file='/dev/urandom', access="stream", &
                form="unformatted", action="read", status="old", iostat=ios)
-  if (ios/=0) error stop 'failed to open random source generator'
   
-  read(u,iostat=ios) seed
-  if (ios/=0) error stop 'failed to read random source generator'
-  
-  close(u)
-  
-  call random_seed(put=seed)
-  
-  
+  if (ios==0) then
+    read(u,iostat=ios) seed
+    if (ios/=0) error stop 'failed to read random source generator'
+    close(u)
+    
+    call random_seed(put=seed)
+  else
+    call random_seed()   ! gfortran and ifort compilers pick a fresh seed.
+    call random_seed(get=check)
+    call random_seed()
+    call random_seed(get=seed)
+    if (all(check==seed)) write(error_unit,*) 'WARNING: your compiler is not picking a unique seed'
+  endif
+    
   print *,'seed: ',seed    ! for debug/test
 
 end subroutine
