15332fa6c9f04ff7a3ac16104778b4bc059e6266
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-10-20 23:27:13 -0400

    init [ci skip]

---
+++ f2018errorstop.f90
@@ -0,0 +1,17 @@
+! checks if compiler can handle F2018 Error Stop with variable
+program f2018errorstop
+
+use, intrinsic:: iso_fortran_env
+implicit none
+
+real :: r
+character(10) :: c
+
+call random_number(r)
+write(c,'(f10.3)') r
+
+print *,compiler_version()
+
+error stop c
+
+end program
