de2290913333ab50a19001e14fcf3ce0ddf4cc38
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-11-29 14:51:47 -0500

    better coarray cmake [skip ci]

--- cmake/Modules/FindCoarray.cmake
+++ cmake/Modules/FindCoarray.cmake
@@ -10,27 +10,15 @@ Finds compiler flags or library necessary to support Fortran 2008/2018 coarrays.
 This packages primary purposes are:
 
 * for compilers natively supporting Fortran coarrays without needing compiler options, simply indicating Coarray_FOUND  (example: Cray)
-* for compilers with built-in Fortran coarray support, present compiler options to enable (example: Intel Fortran)
+* for compilers with built-in Fortran coarray support, enable compiler option (example: Intel Fortran)
 * for compilers needing a library such as OpenCoarrays, presenting library (example: GNU)
 
 
-Module Input Variables
-^^^^^^^^^^^^^^^^^^^^^^
-
-Users or projects may set the following variables to configure the module behaviour:
-
-:variable:`Coarray_ROOT`
-  the root of the coarray library installation.
-
-Variables defined by the module
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-Result variables
-""""""""""""""""
+Result Variables
+^^^^^^^^^^^^^^^^
 
 ``Coarray_FOUND``
-  ``TRUE`` if coarray support is found (whether built-in or library), ``FALSE`` otherwise.
-
+  indicates coarray support found (whether built-in or library)
 
 ``Coarray_LIBRARIES``
   coarray library path
@@ -41,7 +29,15 @@ Result variables
 ``Coarray_MAX_NUMPROCS``
   maximum number of parallel processes
 ``Coarray_NUMPROC_FLAG``
-  use as ${Coarray_EXECUTABLE} ${Coarray_NUMPROC_FLAG} ${Coarray_MAX_NUMPROCS}
+  use for executing in parallel: ${Coarray_EXECUTABLE} ${Coarray_NUMPROC_FLAG} ${Coarray_MAX_NUMPROCS} ${CMAKE_CURRENT_BINARY_DIR}/myprogram
+  
+Cache Variables
+^^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Coarray_LIBRARY``
+  The coarray libraries, if needed and found
 #]=======================================================================]
 
 cmake_policy(VERSION 3.3)
@@ -51,13 +47,16 @@ set(opencoarray_supported GNU)  # future: Flang, etc.
 
 unset(Coarray_COMPILE_OPTIONS)
 unset(Coarray_LIBRARY)
+unset(Coarray_REQUIRED_VARS)
 
 if(CMAKE_Fortran_COMPILER_ID IN_LIST options_coarray)
 
   if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
     set(Coarray_COMPILE_OPTIONS -coarray=shared)
-    set(Coarray_LIBRARY -coarray=shared)  # yes ifort requires it twice
+    set(Coarray_LIBRARY -coarray=shared)  # ifort requires it at build AND link
   endif()
+  
+  list(APPEND Coarray_REQUIRED_VARS ${Coarray_LIBRARY})
 
 elseif(CMAKE_Fortran_COMPILER_ID IN_LIST opencoarray_supported)
 
@@ -72,8 +71,11 @@ elseif(CMAKE_Fortran_COMPILER_ID IN_LIST opencoarray_supported)
     ProcessorCount(Nproc)
     set(Coarray_MAX_NUMPROCS ${Nproc})
     set(Coarray_NUMPROC_FLAG -np)
+    
+    list(APPEND Coarray_REQUIRED_VARS ${Coarray_LIBRARY})
   elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
     set(Coarray_COMPILE_OPTIONS -fcoarray=single)
+    list(APPEND Coarray_REQUIRED_VARS ${Coarray_COMPILE_OPTIONS})
   endif()
 
 endif()
@@ -86,15 +88,16 @@ check_fortran_source_compiles("program cs; real :: x[*]; end" f08coarray SRC_EXT
 unset(CMAKE_REQUIRED_FLAGS)
 unset(CMAKE_REQUIRED_LIBRARIES)
 
+list(APPEND Coarray_REQUIRED_VARS ${f08coarray})
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Coarray
-  REQUIRED_VARS Coarray_LIBRARY f08coarray)
+  REQUIRED_VARS Coarray_REQUIRED_VARS)
 
 set(Coarray_LIBRARIES ${Coarray_LIBRARY})
 
 
 mark_as_advanced(
   Coarray_LIBRARY
-  Coarray_LIBRARIES
-  Coarray_COMPILE_OPTIONS
+  Coarray_REQUIRED_VARS
 )
