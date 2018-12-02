adae518823114a9fb1f52312b4ecb47e30fdae96
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-02 10:20:21 -0500

    doc MPI opts

--- mpi/mpi.cmake
+++ mpi/mpi.cmake
@@ -1,10 +1,17 @@
- # for Gfortran + OpenMPI: 
+# for Gfortran + OpenMPI:
 #    cmake -DMPI_Fortran_COMPILER=/usr/bin/mpif90.openmpi ..
 
   # DON'T USE WRAPPER COMPILER AND FLAGS -- one or the other!
   #set(CMAKE_Fortran_COMPILER ${MPI_Fortran_COMPILER}) #wrapper compiler mpif90
   # consider mpif90 --showme
   # https://www.open-mpi.org/faq/?category=mpi-apps
+
+# --- determine MPI library versions (distinct from MPI version)
+# can cause MPI to falsely fail
+# didn't work (blank output) with MPI 4.0.0 and GNU
+
+#set(MPI_DETERMINE_LIBRARY_VERSION true)
+#message(STATUS "MPI Library version: " ${MPI_Fortran_LIBRARY_VERSION})
 
 # --- verify MPI actually works
 set(CMAKE_REQUIRED_FLAGS ${MPI_Fortran_COMPILE_OPTIONS})
@@ -14,9 +21,9 @@ include(CheckFortranSourceCompiles)
 check_fortran_source_compiles("program a; use mpi; end" hasMPI
                               SRC_EXT f90)
 if(NOT hasMPI)
-  message(STATUS "MPI library not functioning with " 
+  message(STATUS "MPI library not functioning with "
           ${CMAKE_Fortran_COMPILER_ID} " " ${CMAKE_Fortran_COMPILER_VERSION})
   return()
-endif()  
+endif()
 
 # --- end verify MPI
