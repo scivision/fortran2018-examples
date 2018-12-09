7454c3a51ee093f391790aad5cbb2cba194c74e6
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-08 23:27:01 -0500

    init mkdir
    correct logic
    
    correct logic

--- standard/README.md
+++ standard/README.md
@@ -4,5 +4,6 @@
 Examples of standard and non-standard coding
 
 * statement_function.f90: making statement function into regular function
-* sleep_std.f90: standard way to have fractional second sleep
-* replace_pause.f90: replace pause with standard statements.
+* sleep.f90: standard way to have fractional second sleep
+* pause.f90: replace pause with standard statements.
+* mkdir.f90: mkdir() is standard Fortran using C
---
+++ standard/mkdir.f90
@@ -0,0 +1,65 @@
+program std_mkdir
+!! Fortran standard compliant mkdir().
+!! mkdir() is a GNU extension, not standard Fortran.
+use, intrinsic:: iso_c_binding, only: c_int, c_char
+use, intrinsic:: iso_fortran_env, only: stderr=>error_unit
+implicit none
+
+!> use this interface in your module
+interface
+integer(c_int) function mkdir_c(path, mask) bind (C, name='mkdir')
+  import c_int, c_char
+  character(kind=c_char), intent(in) :: path(*)
+  integer(c_int), value, intent(in) :: mask
+end function mkdir_c
+end interface
+
+!> demo
+character(4096) :: buf
+
+call get_command_argument(1,buf)
+
+call mkdir(trim(buf))
+
+contains
+
+subroutine mkdir(path)
+!! create a directory, with parents if needed
+!! N.B. inquire() does not work for directories by Fortran standard
+
+integer(c_int) :: ret
+integer :: i,i0
+character(len=*), intent(in) :: path
+character(kind=c_char, len=:), allocatable :: buf  
+!! must use allocatable buffer, not direct substring to C
+
+
+if (len(path) == 0) error stop 'must specify directory to create'
+
+!> single directory
+i = index(path, '/')
+if (i==0) then
+  ret = mkdir_c(path, int(o'755', c_int))
+  return
+endif
+
+!> parents
+i=-1; i0=1
+do while( i /= 0 )
+  i = index(path(i0:), '/')
+  if (i /= 0) then
+    i0 = i0+i
+  else
+    i0 = len(path)+1
+  endif
+ 
+  !> allocated string buffer necessary for C interface
+  buf = path(1:i0-1)
+
+  ret = mkdir_c(buf, int(o'755', c_int))
+
+  deallocate(buf)
+enddo
+
+end subroutine mkdir
+end program
