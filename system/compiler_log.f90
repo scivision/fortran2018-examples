0518ae70b850ca6b3549ff96ad7a36fc202d418a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-09 01:38:23 -0500

    int

---
+++ system/compiler_log.f90
@@ -0,0 +1,19 @@
+program complog
+call compiler_log('./compiler.log')
+end program
+
+
+subroutine compiler_log(logpath)
+use, intrinsic :: iso_fortran_env, only: compiler_version, compiler_options
+
+character(*), intent(in) :: logpath
+integer :: u
+
+open(newunit=u, file=logpath, form='formatted', status='unknown', action='write')
+
+write(u,'(A,/)') compiler_version()
+write(u,'(A)') compiler_options()
+
+close(u)
+
+end subroutine compiler_log
