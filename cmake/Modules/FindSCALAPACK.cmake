6cbdd1a65ec4e563359b193ead84f7a39184c815
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-11 07:22:10 -0500

    template [ci skip]

--- cmake/Modules/FindSCALAPACK.cmake
+++ cmake/Modules/FindSCALAPACK.cmake
@@ -7,13 +7,17 @@ unset(SCALAPACK_LIBRARY)
 
 if(USEMKL OR CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
 
-  find_package(Threads QUIET REQUIRED)
-
   # FIXME: this would be for threaded
   # mkl_scalapack_lp64 mkl_intel_lp64 mkl_intel_thread mkl_core mkl_blacs_intelmpi_lp64 iomp5
+  
+  if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
+    set(slibs mkl_scalapack_lp64 mkl_intel_lp64 mkl_sequential mkl_core mkl_blacs_intelmpi_lp64)
+  else()
+    set(slibs mkl_scalapack_lp64 mkl_gf_lp64 mkl_sequential mkl_core mkl_blacs_openmpi_lp64)
+  endif()
 
   # this is for sequential:
-  foreach(slib mkl_scalapack_lp64 mkl_intel_lp64 mkl_sequential mkl_core mkl_blacs_intelmpi_lp64)
+  foreach(slib ${slibs})
     find_library(SCALAPACK_${slib}_LIBRARY
              NAMES ${slib}
              PATHS $ENV{MKLROOT}/lib
@@ -21,13 +25,13 @@ if(USEMKL OR CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
                    $ENV{INTEL}/mkl/lib/intel64
              NO_DEFAULT_PATH)
     if(NOT SCALAPACK_${slib}_LIBRARY)
-      message(FATAL_ERROR "NOT FOUND: " ${slib} ${SCALAPACK_${slib}_LIBRARY})
+      message(FATAL_ERROR "NOT FOUND: " ${slib})
     endif()
-#    message(STATUS "Intel MKL Scalapack FOUND: " ${slib} ${SCALAPACK_${slib}_LIBRARY})
+    message(STATUS "Intel MKL Scalapack FOUND: " ${slib})
     list(APPEND SCALAPACK_LIBRARY ${SCALAPACK_${slib}_LIBRARY})
     mark_as_advanced(SCALAPACK_${slib}_LIBRARY)
   endforeach()
-  list(APPEND SCALAPACK_LIBRARY ${CMAKE_THREAD_LIBS_INIT} ${CMAKE_DL_LIBS} m)
+  list(APPEND SCALAPACK_LIBRARY pthread ${CMAKE_DL_LIBS} m)
 else()
   find_package(PkgConfig QUIET)
   pkg_check_modules(PC_SCALAPACK QUIET SCALAPACK)
