1e1227a96fa9ada124cec5d3551d772e3b5480d7
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-04-05 15:45:49 -0400

    add leading zeros example, temp disable options needing Meson PR merge

---
+++ io/leading_zeros.f90
@@ -0,0 +1,17 @@
+!! Print leading zeros for real, integer in Fortran
+use, intrinsic :: iso_fortran_env, only: stdout=>output_unit, dp=>real64
+
+implicit none
+
+real(dp):: x
+integer :: i
+
+x = 1234.56_dp
+i = 123456
+
+write(stdout,'(a5,f12.7)') 'hello',x
+write(stdout,'(i0.12)')  i
+
+print *,'there should be leading zeros and no blanks for the real and integer above.'
+
+end program
