558ace644c18b4e13b9630e61f8be779b59aa586
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-05-28 21:47:37 -0400

    fortran 2018 prpoerties

---
+++ real/properties.f90
@@ -0,0 +1,22 @@
+!! Show:
+!! * %re and %im properties of real number in Fortran 2018
+!! * %len property of character
+
+use, intrinsic :: iso_fortran_env, only: real32
+implicit none
+
+character(:), allocatable :: c
+complex :: z
+
+z = (1, 1.414)
+c = 'hello'
+c = 'hi'
+
+print *, c, c%len
+if (c%len /= len(c)) error stop 'character %len'
+
+
+print *, z%re, z%im, z%kind
+if (real32 /= z%kind) error stop 'kind mistmatch'
+
+end program
