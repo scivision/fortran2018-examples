da30e51d6226f508ed2c5fa7cbd575e3f1747472
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-30 23:25:42 -0400

    setup.cmake template

--- setup.cmake
+++ setup.cmake
@@ -3,36 +3,23 @@
 
 # --- Project-specific -Doptions
 # these will be used if the project isn't already configured.
+set(_opts)
 
 # --- boilerplate follows
+message(STATUS "CMake ${CMAKE_VERSION}")
+if(CMAKE_VERSION VERSION_LESS 3.15)
+  message(FATAL_ERROR "Please update CMake >= 3.15")
+endif()
+
 # site is OS name
 if(NOT DEFINED CTEST_SITE)
   set(CTEST_SITE ${CMAKE_SYSTEM_NAME})
 endif()
 
-# if compiler specified, deduce its ID
+# test name is Fortran compiler in FC
+# Note: ctest scripts cannot read cache variables like CMAKE_Fortran_COMPILER
 if(DEFINED ENV{FC})
-  set(FC $ENV{FC})
-endif()
-if(DEFINED CMAKE_Fortran_COMPILER)
-  set(FC ${CMAKE_Fortran_COMPILER})
-endif()
-if(DEFINED FC)
-  foreach(c gfortran ifort flang pgfortran nagfor xlf ftn)
-    string(FIND ${FC} ${c} i)
-    if(i GREATER_EQUAL 0)
-      if(c STREQUAL gfortran)
-        execute_process(COMMAND gfortran -dumpversion
-          RESULT_VARIABLE _ret
-          OUTPUT_VARIABLE _vers OUTPUT_STRIP_TRAILING_WHITESPACE)
-        if(_ret EQUAL 0)
-          string(APPEND c "-${_vers}")
-        endif()
-      endif()
-      set(CTEST_BUILD_NAME ${c})
-      break()
-    endif()
-  endforeach()
+  set(CTEST_BUILD_NAME $ENV{FC})
 endif()
 
 if(NOT DEFINED CTEST_BUILD_CONFIGURATION)
@@ -44,16 +31,17 @@ if(NOT DEFINED CTEST_BINARY_DIRECTORY)
   set(CTEST_BINARY_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/build)
 endif()
 
+# CTEST_CMAKE_GENERATOR must be defined in any case here.
 if(NOT DEFINED CTEST_CMAKE_GENERATOR)
   find_program(_gen NAMES ninja ninja-build samu)
   if(_gen)
     set(CTEST_CMAKE_GENERATOR "Ninja")
   elseif(WIN32)
     set(CTEST_CMAKE_GENERATOR "MinGW Makefiles")
-    set(CTEST_BUILD_FLAGS -j)
+    set(CTEST_BUILD_FLAGS -j)  # not --parallel as this goes to generator directly
   else()
     set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-    set(CTEST_BUILD_FLAGS -j)
+    set(CTEST_BUILD_FLAGS -j)  # not --parallel as this goes to generator directly
   endif()
 endif()
 
@@ -63,4 +51,12 @@ if(NOT EXISTS ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt)
 endif()
 ctest_build(BUILD ${CTEST_BINARY_DIRECTORY} CONFIGURATION ${CTEST_BUILD_CONFIGURATION})
 ctest_test(BUILD ${CTEST_BINARY_DIRECTORY})
-# ctest_submit()
+
+# using ctest_submit makes error code 0 even if test(s) failed!
+if(DEFINED ENV{CI})
+  set(CI $ENV{CI})
+endif()
+
+if(NOT CI)
+  # ctest_submit()
+endif()
