d221b14d20d003a38e35fd07b73bfd6774cdc88e
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-07 14:06:15 -0400

    mpi.cmake template

--- mpi/mpi.cmake
+++ mpi/mpi.cmake
@@ -1,43 +1,56 @@
-# for Gfortran + OpenMPI:
-#    cmake -DMPI_Fortran_COMPILER=/usr/bin/mpif90.openmpi ..
-
-  # DON'T USE WRAPPER COMPILER AND FLAGS -- one or the other!
-  #set(CMAKE_Fortran_COMPILER ${MPI_Fortran_COMPILER}) #wrapper compiler mpif90
-  # consider mpif90 --showme
-  # https://www.open-mpi.org/faq/?category=mpi-apps
-
-# --- determine MPI library versions (distinct from MPI version)
-# can cause MPI to falsely fail
-# didn't work (blank output) with MPI 4.0.0 and GNU
-
-#set(MPI_DETERMINE_LIBRARY_VERSION true)
-#message(STATUS "MPI Library version: " ${MPI_Fortran_LIBRARY_VERSION})
-
-# --- verify MPI actually works
-find_package(MPI COMPONENTS Fortran)
-find_package(Threads)
-
-if(NOT MPI_Fortran_FOUND)
+find_package(MPI COMPONENTS C Fortran)
+if(NOT MPI_FOUND)
   return()
 endif()
+find_package(Threads)
 
-set(CMAKE_REQUIRED_FLAGS ${MPI_Fortran_COMPILE_OPTIONS})
-set(CMAKE_REQUIRED_INCLUDES ${MPI_Fortran_INCLUDE_DIRS})
-set(CMAKE_REQUIRED_LIBRARIES ${MPI_Fortran_LIBRARIES} Threads::Threads)
+set(CMAKE_REQUIRED_INCLUDES)
+set(CMAKE_REQUIRED_FLAGS)
+
+# --- test Fortran MPI
+
+set(CMAKE_REQUIRED_LIBRARIES MPI::MPI_Fortran Threads::Threads)
 include(CheckFortranSourceCompiles)
 
 # Windows Intel 2019 MPI doesn't yet have mpi_f08
-check_fortran_source_compiles("use mpi; end" hasMPI SRC_EXT F90)
+if(NOT DEFINED MPI_Fortran_OK)
+  check_fortran_source_compiles("use mpi; end" MPI_Fortran_OK SRC_EXT F90)
 
-message(STATUS "
-Libs: ${MPI_Fortran_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT}
-Include: ${MPI_Fortran_INCLUDE_DIRS}
-Opts: ${MPI_Fortran_COMPILE_OPTIONS} ${MPI_Fortran_LINK_FLAGS}
-MPIexec: ${MPIEXEC_EXECUTABLE}"
-)
-
-if(NOT hasMPI)
-  message(STATUS "MPI library mpif.h not functioning with ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
+  message(STATUS "Fortran MPI:
+  Libs: ${MPI_Fortran_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT}
+  Include: ${MPI_Fortran_INCLUDE_DIRS}
+  MPIexec: ${MPIEXEC_EXECUTABLE}"
+  )
 endif()
 
-# --- end verify MPI
+if(NOT MPI_Fortran_OK)
+  message(FATAL_ERROR "MPI Fortran library mpif.h not functioning with ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
+endif()
+
+# --- test C MPI
+
+set(CMAKE_REQUIRED_LIBRARIES MPI::MPI_C Threads::Threads)
+include(CheckCSourceCompiles)
+
+if(NOT DEFINED MPI_C_OK)
+  check_c_source_compiles("
+#include <mpi.h>
+#ifndef NULL
+#define NULL 0
+#endif
+int main(void) {
+    MPI_Init(NULL, NULL);
+    MPI_Finalize();
+    return 0;}
+" MPI_C_OK)
+
+  message(STATUS "C MPI:
+  Libs: ${MPI_C_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT}
+  Include: ${MPI_C_INCLUDE_DIRS}
+  MPIexec: ${MPIEXEC_EXECUTABLE}"
+  )
+endif()
+
+if(NOT MPI_C_OK)
+  message(FATAL_ERROR "MPI C library mpi.h not functioning with ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION}")
+endif()
