19a0ad91fc3e4ae1ccb4920cf114815cce6d2eeb
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-12 16:12:35 -0400

    setup.cmake parallel

--- setup.cmake
+++ setup.cmake
@@ -16,6 +16,11 @@ if(NOT DEFINED CTEST_SITE)
   set(CTEST_SITE ${CMAKE_SYSTEM_NAME})
 endif()
 
+# parallel test--use ctest_test(PARALLEL_LEVEL ${Ncpu} as setting CTEST_PARALLEL_LEVEL has no effect
+include(ProcessorCount)
+ProcessorCount(Ncpu)
+message(STATUS "${Ncpu} CPU cores detected")
+
 # test name is Fortran compiler in FC
 # Note: ctest scripts cannot read cache variables like CMAKE_Fortran_COMPILER
 if(DEFINED ENV{FC})
@@ -84,9 +89,14 @@ if(return_code EQUAL 0 AND Nerror EQUAL 0 AND cmake_err EQUAL 0)
   BUILD ${CTEST_BINARY_DIRECTORY}
   RETURN_VALUE return_code
   CAPTURE_CMAKE_ERROR ctest_err
+  PARALLEL_LEVEL ${Ncpu}
   )
 else()
   message(STATUS "SKIP: ctest_test(): returncode: ${return_code}; CMake error code: ${cmake_err}")
 endif()
 
 # ctest_submit()
+
+if(NOT (return_code EQUAL 0 AND Nerror EQUAL 0 AND cmake_err EQUAL 0 AND ctest_err EQUAL 0))
+  message(FATAL_ERROR "Build and test failed.")
+endif()
