ce1e5ab1898306d4dbd504e55b75fff1ba01a9de
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-06 20:07:08 -0400

    move C++/C/Fortran interface to https://github.com/scivision/fortran-c-cpp-interface

--- cxx/call_c.c
+++
@@ -1,4 +0,0 @@
-void timestwo(double x[], double x2[], int N){
-  for (int i=0; i<N; i++)
-    x2[i] = x[i] * 2;
-}
--- setup.cmake
+++ setup.cmake
@@ -8,7 +8,8 @@ set(_opts)
 # --- boilerplate follows
 message(STATUS "CMake ${CMAKE_VERSION}")
 if(CMAKE_VERSION VERSION_LESS 3.15)
-  message(FATAL_ERROR "Please update CMake >= 3.15")
+  message(FATAL_ERROR "Please update CMake >= 3.15.
+    Try 'pip install -U cmake' or https://cmake.org/download/")
 endif()
 
 # site is OS name
@@ -49,11 +50,21 @@ if(NOT DEFINED CTEST_BINARY_DIRECTORY)
 endif()
 
 # CTEST_CMAKE_GENERATOR must be defined in any case here.
-if(NOT DEFINED CTEST_CMAKE_GENERATOR)
+if(NOT DEFINED CTEST_CMAKE_GENERATOR AND CMAKE_VERSION VERSION_GREATER_EQUAL 3.17)
   find_program(_gen NAMES ninja ninja-build samu)
   if(_gen)
-    set(CTEST_CMAKE_GENERATOR "Ninja")
-  elseif(WIN32)
+    execute_process(COMMAND ${_gen} --version
+      OUTPUT_VARIABLE _ninja_version
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      RESULT_VARIABLE _gen_ok
+      TIMEOUT 10)
+    if(_gen_ok EQUAL 0 AND _ninja_version VERSION_GREATER_EQUAL 1.10)
+      set(CTEST_CMAKE_GENERATOR "Ninja")
+    endif()
+  endif(_gen)
+endif()
+if(NOT DEFINED CTEST_CMAKE_GENERATOR)
+  if(WIN32)
     set(CTEST_CMAKE_GENERATOR "MinGW Makefiles")
     set(CTEST_BUILD_FLAGS -j)  # not --parallel as this goes to generator directly
   else()
