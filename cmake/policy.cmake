fccbb58f19321827245aa3c872ca72af62dc60a7
Michael Hirsch <scivision@users.noreply.github.com>
2020-11-22 14:28:08 -0500

    cmake policy template

--- cmake/policy.cmake
+++ cmake/policy.cmake
@@ -1,9 +1,7 @@
+set(CMAKE_EXPORT_COMPILE_COMMANDS on)
+
 if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
   message(FATAL_ERROR "use cmake -B build or similar to avoid building in-source, which is messy")
-endif()
-
-if(CMAKE_VERSION VERSION_EQUAL 3.19.0-rc1)
-  message(FATAL_ERROR "CMake 3.19.0-rc1 has breaking bugs for any project. Please use a different CMake version.")
 endif()
 
 if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.20)
