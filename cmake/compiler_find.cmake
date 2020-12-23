b9d07fb49167e18d4f6f5907b6d54ccbff817b68
Michael Hirsch <scivision@users.noreply.github.com>
2020-12-22 22:12:57 -0500

    compiler_find.cmake template

--- cmake/compiler_find.cmake
+++ cmake/compiler_find.cmake
@@ -8,14 +8,15 @@ set(CMAKE_CONFIGURATION_TYPES "Release;RelWithDebInfo;Debug" CACHE STRING "Build
 
 # Help CMake find matching compilers, especially needed for MacOS
 
-function(find_compilers)
-
 if(APPLE)
   set(_paths /usr/local/bin /opt/homebrew/bin)
   # for Homebrew that's not on PATH (can be an issue on CI)
 else()
   set(_paths)
 endif()
+
+
+function(find_fortran)
 
 if(NOT DEFINED ENV{FC})
   # temporarily removed ifort, because Intel oneAPI release Dec 8, 2020 is broken for HDF5 and MPI in general.
@@ -27,7 +28,7 @@ if(NOT DEFINED ENV{FC})
   endif()
 endif()
 
-if(NOT DEFINED ENV{FC} OR DEFINED ENV{CC})
+if(NOT DEFINED ENV{FC})
   return()
 endif()
 # ensure FC exists as a executable program
@@ -35,33 +36,59 @@ find_program(FC
   NAMES $ENV{FC}
   PATHS ${_paths})
 
-if(NOT FC)
-  return()
+if(FC)
+  set(FC ${FC} PARENT_SCOPE)
 endif()
 
-# remember, Apple has "/usr/bin/gcc" which is really clang
-# the technique below is NECESSARY to work on Mac and not find the wrong GCC
-get_filename_component(_dir ${FC} DIRECTORY)
+endfunction(find_fortran)
 
-# use same compiler for C and Fortran, which CMake might not do itself
-if(FC MATCHES ".*ifort")
-  if(WIN32)
-    set(_name icl)
-  else()
-    set(_name icc)
+
+function(find_c)
+
+if(NOT FC AND DEFINED ENV{FC})
+  set(FC $ENV{FC})
+endif()
+
+message(VERBOSE " FC hint: ${FC}")
+
+if(DEFINED ENV{CC})
+  set(_name $ENV{CC})
+  set(_dir)
+else()
+  # remember, Apple has "/usr/bin/gcc" which is really clang
+  # the technique below is NECESSARY to work on Mac and not find the wrong GCC
+  if(FC)
+    get_filename_component(_dir ${FC} DIRECTORY)
   endif()
-elseif(FC MATCHES ".*gfortran")
-  set(_name gcc gcc-11 gcc-10 gcc-9 gcc-8 gcc-7)
+  # use same compiler for C and Fortran, which CMake might not do itself
+  if(FC MATCHES ".*ifort")
+    if(WIN32)
+      set(_name icl)
+    else()
+      set(_name icc)
+    endif()
+  elseif(FC MATCHES ".*gfortran")
+    set(_name gcc gcc-11 gcc-10 gcc-9 gcc-8 gcc-7)
+  else()
+    return()
+  endif()
 endif()
 
-find_program(CC NAMES ${_name}
-HINTS ${_dir}
-NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
+find_program(CC
+  NAMES ${_name}
+  HINTS ${_dir}
+  NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
 
 if(CC)
+  set(CC ${CC} PARENT_SCOPE)
   set(ENV{CC} ${CC})
+  message(VERBOSE " CC hint: ${CC}")
 endif()
 
-endfunction(find_compilers)
+endfunction(find_c)
 
-find_compilers()
+
+function(find_c_fortran)
+find_fortran()
+find_c()
+endfunction(find_c_fortran)
