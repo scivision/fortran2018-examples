f0fb427d0bc45e0a4badd1dd97cba9572dcf9c96
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-18 14:15:58 -0500

    nan: better formatted output

--- real/nans.f90
+++ real/nans.f90
@@ -1,49 +1,67 @@
 program nans
-! Note that -Ofast and -ffast-math break this program--
-! NaN are not IEEE compiliant if using -Ofast or -ffast-math.
+! Compiler options like -Ofast and -ffast-math break this program.
+! NaN are not IEEE compliant if using such options.
 !
 ! Gfortran >= 6 needed for ieee_arithmetic: ieee_is_nan
 
-!use, intrinsic :: iso_c_binding, only: sp=>C_FLOAT, dp=>C_DOUBLE, qp=>C_LONG_DOUBLE
-! more precision for real128,complex256 by using iso_fortran_env vs.  iso_c_binding
-use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64, qp=>real128
+!> Examples
+
+!> Gfortran 10.2.0 Windows
+!       IEEE   value   isnan                         hex
+!     real 32    NaN      T                          FFC00000
+!     real 64    NaN      T                  FFF8000000000000
+!     real128    NaN      T  FFFF8000000000000000000000000000
+!  complex 64    NaN      T                          FFC00000
+!  complex128    NaN      T                  FFF8000000000000
+!  complex256    NaN      T  FFFF8000000000000000000000000000
+
+!> Ifort 2021.1 Winodws and Ifx 2021.1 Windows
+!       IEEE   value   isnan                         hex
+!     real 32    NaN      T                          7FC00000
+!     real 64    NaN      T                  7FF8000000000000
+!     real128    NaN      T  7FFF8000000000000000000000000000
+!  complex 64    NaN      T                          7FC00000
+!  complex128    NaN      T                  7FF8000000000000
+!  complex256    NaN      T  7FFF8000000000000000000000000000
+
+use, intrinsic :: iso_fortran_env, only: real32, real64, real128
 use, intrinsic :: iso_fortran_env, only: int32, int64
 use, intrinsic :: ieee_arithmetic, only: ieee_value, ieee_quiet_nan, ieee_is_nan
 implicit none (type, external)
 
-real(sp) :: nan_sp
-real(dp) :: nan_dp
-real(qp) :: nan_qp
-complex(dp) :: nan_zp
-complex(qp) :: nan_zqp
+real(real32) :: nan_real32
+real(real64) :: nan_real64
+real(real128) :: nan_real128
+complex(real32) :: nan_complex64
+complex(real64) :: nan_complex128
+complex(real128) :: nan_complex256
 
 integer(int32) :: ISP
 integer(int64) :: IDP
+character(*), parameter :: fmt='(A8,I3,2X,F5.1,6X,L1,2X,Z32)'
 
 
 ! this is the cross platform way to get NaN on modern compilers including gfortran and ifort.
-nan_sp = ieee_value(1.,ieee_quiet_nan)
-nan_dp = ieee_value(1.,ieee_quiet_nan)
-nan_qp = ieee_value(1.,ieee_quiet_nan)
-nan_zp = ieee_value(1.,ieee_quiet_nan)
-nan_zqp = ieee_value(1.,ieee_quiet_nan)
+nan_real32 = ieee_value(0.,ieee_quiet_nan)
+nan_real64 = ieee_value(0.,ieee_quiet_nan)
+nan_real128 = ieee_value(0.,ieee_quiet_nan)
+nan_complex64 = ieee_value(0., ieee_quiet_nan)
+nan_complex128 = ieee_value(0.,ieee_quiet_nan)
+nan_complex256 = ieee_value(0.,ieee_quiet_nan)
 
 ! --------- print results
 
-ISP = transfer(nan_sp, ISP)
-IDP = transfer(nan_dp, IDP)
+ISP = transfer(nan_real32, ISP)
+IDP = transfer(nan_real64, IDP)
 
-print *,'IEEE  value  isnan  hex'
-print '(A4,2X,F5.1,6X,L1,2X,Z32)','sp',nan_sp, ieee_is_nan(nan_sp), ISP
-print '(A4,2X,F5.1,6X,L1,2X,Z32)','dp',nan_dp, ieee_is_nan(nan_dp), IDP
-print '(A4,2X,F5.1,6X,L1,2X,Z32)','qp',nan_qp, ieee_is_nan(nan_qp), nan_qp
+print '(A10,A8,A8,A28)','IEEE', 'value', 'isnan', 'hex'
+print fmt,'real',storage_size(nan_real32), nan_real32, ieee_is_nan(nan_real32), ISP
+print fmt,'real',storage_size(nan_real64), nan_real64, ieee_is_nan(nan_real64), IDP
+print fmt,'real',storage_size(nan_real128),nan_real128, ieee_is_nan(nan_real128), nan_real128
 
-print '(A4,2X,F5.1,6X,L1,2X,Z32)','zp',real(nan_zp),ieee_is_nan(real(nan_zp)),nan_zp
-print '(A4,2X,F5.1,6X,L1,2X,Z32)','zqp',real(nan_zqp),ieee_is_nan(real(nan_zqp)),nan_zqp
+print fmt,'complex',storage_size(nan_complex64),real(nan_complex64),ieee_is_nan(real(nan_complex64)),nan_complex64
+print fmt,'complex',storage_size(nan_complex128),real(nan_complex128),ieee_is_nan(real(nan_complex128)),nan_complex128
+print fmt,'complex',storage_size(nan_complex256),real(nan_complex256),ieee_is_nan(real(nan_complex256)),nan_complex256
 
-
-! for single prec.:
-! gfortran 8.0: FFC00000
-! ifort 18.0: 7FC00000
 
 end program
