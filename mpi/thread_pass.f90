fcf09fbdd08930aef1caefa33e81371288ea8f9b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-05-04 14:10:32 -0400

    MPI: thread_pass mpi 3.0 / mpi_f08 style

--- mpi/thread_pass.f90
+++ mpi/thread_pass.f90
@@ -1,74 +1,73 @@
 program mpi_pass
 !! passes data between two threads
-!!  Author:  John Burkardt
-use, intrinsic :: iso_fortran_env, only: sp=>real32, compiler_version
-use mpi, only : MPI_STATUS_SIZE, mpi_comm_world, mpi_init, mpi_get_count, &
-  mpi_real, mpi_any_source, mpi_any_tag, mpi_source, mpi_tag, mpi_comm_size
+!! This would be much simpler using Fortran 2008 coarray syntax
+!!
+!!  Original author:  John Burkardt
+use, intrinsic :: iso_fortran_env, only: real32, compiler_version, int64
+use mpi_f08, only : mpi_status, mpi_comm_world, mpi_init, mpi_get_count, &
+  mpi_real, mpi_any_source, mpi_any_tag, mpi_source, mpi_tag, mpi_comm_size, &
+  mpi_comm_rank, mpi_recv, mpi_send, mpi_finalize
 
 implicit none (type, external)
 
-character(10) :: time
 integer :: mcount
-real(sp) :: dat(0:99), val(200)
-integer :: dest, i, num_procs, rank, tag, ierr, status(MPI_STATUS_SIZE)
-! type(MPI_STATUS) :: status  !< MPI_F08
+real(real32) :: dat(0:99), val(200)
+integer :: dest, i, num_procs, id, tag
+integer(int64) :: tic, toc, rate
 
-external :: mpi_recv, mpi_send, mpi_finalize, mpi_comm_rank
+type(MPI_STATUS) :: status
 
-!  Initialize MPI.
-call MPI_Init(ierr)
-if (ierr /= 0) error stop 'mpi init error'
+call system_clock(tic)
 
-!  Determine this process's rank.
-call MPI_Comm_rank ( MPI_COMM_WORLD, rank, ierr)
-if (ierr /= 0) error stop 'mpi rank error'
+call MPI_Init()
+
+!  Determine this process's ID.
+call MPI_Comm_rank(MPI_COMM_WORLD, id)
 
 !  Find out the number of processes available.
-call MPI_Comm_size ( MPI_COMM_WORLD, num_procs, ierr)
-if (ierr /= 0) error stop 'mpi size error'
+call MPI_Comm_size(MPI_COMM_WORLD, num_procs)
+
 if (num_procs < 2) error stop 'two threads are required, use: mpiexec -np 2 ./mpi_pass'
 
-!  Have Process 0 say hello.
-if ( rank == 0 ) then
+if (id == 0) then
   print *,compiler_version()
-  print *, '  An MPI test program. number of processes available ', num_procs
+  print *, 'Number of MPI processes available ', num_procs
 end if
 
 !  Process 0 expects to receive as much as 200 real values, from any source.
-if ( rank == 0 ) then
-  call MPI_Recv (val, size(val), MPI_REAL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status, ierr)
-  if (ierr /= 0) error stop 'mpi receive error'
+select case (id)
+case (0)
+  print *, id, "waiting for MPI_send() from image 1"
+  call MPI_Recv (val, size(val), MPI_REAL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status)
 
-  print *, rank, ' Got data from processor ', status(MPI_SOURCE), 'tag',status(MPI_TAG)
+  print *, id, ' Got data from processor ', status%MPI_SOURCE, 'tag',status%MPI_TAG
 
-  call MPI_Get_count ( status, MPI_REAL, mcount, ierr)
+  call MPI_Get_count(status, MPI_REAL, mcount)
 
-  print *, rank, ' Got ', mcount, ' elements.'
+  print *, id, ' Got ', mcount, ' elements.'
 
-  print *, rank, ' val(5) = ', val(5)
+  if (val(5) /= 4) error stop "data did not transfer"
 
 !  Process 1 sends 100 real values to process 0.
-else if ( rank == 1 ) then
-  print *, rank, ' - setting up data to send to process 0.'
+case (1)
+  print *, id, ': setting up data to send to process 0.'
 
-  do i = 0, 99
-    dat(i) = real ( i )
-  end do
+  dat = real([(i, i = 0, 99)])
 
   dest = 0
   tag = 55
-  call MPI_Send ( dat, size(dat), MPI_REAL, dest, tag, MPI_COMM_WORLD, ierr)
-  if (ierr /= 0) error stop 'mpi send error'
-else
-  print *, rank, ' - MPI has no work for me!'
-end if
+  call MPI_Send(dat, size(dat), MPI_REAL, dest, tag, MPI_COMM_WORLD)
+case default
+  print *, id, ': MPI has no work for image', id
+end select
 
-call MPI_Finalize(ierr)
-if (ierr /= 0) error stop 'mpi finalize error'
+call MPI_Finalize()
 
-if ( rank == 0 ) then
-  call date_and_time (time = time )
-  print *, '  Normal  execution. ',time
+
+if (id == 0) then
+  call system_clock(toc)
+  call system_clock(count_rate=rate)
+  print *, 'Run time in seconds: ',real(toc - tic)  / rate
 end if
 
 end program
