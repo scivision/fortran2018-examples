b6a86bf9c0a572f9f6c4e11cdaed8775a3405675
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-09 16:20:32 -0400

    cmake template

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -8,7 +8,7 @@ if(NOT abi_ok)
   if(abi_ok)
     message(CHECK_PASS "OK")
   else()
-    message(FATAL ERROR "C compiler {CMAKE_C_COMPILER_ID} {CMAKE_C_COMPILER_VERSION} and Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION} are ABI-incompatible.")
+    message(FATAL_ERROR "ABI-incompatible: C compiler ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION} and Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
   endif()
 endif()
 
