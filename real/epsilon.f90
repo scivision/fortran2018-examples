b1806b03a8e28d67dd5854b4ba8004ed09dd2adb
Michael Hirsch <scivision@users.noreply.github.com>
2021-07-22 00:40:11 -0400

    print ieee_next_after epsilon

--- real/epsilon.f90
+++ real/epsilon.f90
@@ -17,6 +17,9 @@ print *, "epsilon real64", eps64
 one32_eps = ieee_next_after(1._real32, 2.)
 one64_eps = ieee_next_after(1._real64, 2.)
 
+print *, "ieee_next_after(1, 2) eps real32", one32_eps - 1
+print *, "ieee_next_after(1, 2) eps real64", one64_eps - 1
+
 !> this test should be bit exact true
 if (one32_eps - eps32 /= 1) error stop "real32 epsilon not meeting specification"
 if (one64_eps - eps64 /= 1) error stop "real64 epsilon not meeting specification"
