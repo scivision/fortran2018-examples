44cfc184663433f4f002371ff44d4363e8bd97f2
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-31 23:33:42 -0400

    if syntax

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -18,7 +18,7 @@ if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   "$<$<COMPILE_LANGUAGE:Fortran>:-traceback;-heap-arrays>"
   "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-warn;-debug;-check>"
   )
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
 
   add_compile_options(
   $<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>
@@ -27,7 +27,7 @@ elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   )
 
 #   "$<$<COMPILE_LANGAUGE:Fortran>:-Wrealloc-lhs>"  # not -Wrealloc-lhs-all which warns on character
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL NAG)
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "NAG")
   # https://www.nag.co.uk/nagware/np/r70_doc/manual/compiler_2_4.html#OPTIONS
   add_compile_options(
   "$<$<COMPILE_LANGUAGE:Fortran>:-f2018;-C;-colour;-gline;-nan;-info;-u>"
