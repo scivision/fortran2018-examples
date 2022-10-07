7e989a549cb54452b5f1e3a2cf114e5b04916557
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-07 00:46:56 -0400

    put 128-bit nan in own file

--- src/real/nans.f90
+++ src/real/nans.f90
@@ -1,8 +1,6 @@
 program nans
 ! Compiler options like -Ofast and -ffast-math break this program.
 ! NaN are not IEEE compliant if using such options.
-!
-! Gfortran >= 6 needed for ieee_arithmetic: ieee_is_nan
 
 !> Examples
 
@@ -24,13 +22,12 @@ program nans
 !  complex128    NaN      T                  7FF8000000000000
 !  complex256    NaN      T  7FFF8000000000000000000000000000
 
-use, intrinsic :: iso_fortran_env
+use, intrinsic :: iso_fortran_env, only : real32, real64, int32, int64
 use, intrinsic :: ieee_arithmetic, only: ieee_value, ieee_quiet_nan, ieee_is_nan
 implicit none
 
 real(real32) :: nan_real32
 real(real64) :: nan_real64
-complex(real32) :: nan_complex64
 
 integer(int32) :: ISP
 integer(int64) :: IDP
@@ -49,26 +46,5 @@ IDP = transfer(nan_real64, IDP)
 print '(A10,A8,A8,A28)','IEEE', 'value', 'isnan', 'hex'
 print fmt,'real',storage_size(nan_real32), nan_real32, ieee_is_nan(nan_real32), ISP
 print fmt,'real',storage_size(nan_real64), nan_real64, ieee_is_nan(nan_real64), IDP
-
-#ifdef r128
-block
-
-real(real128) :: nan_real128
-complex(real64) :: nan_complex128
-complex(real128) :: nan_complex256
-
-nan_real128 = ieee_value(0.,ieee_quiet_nan)
-nan_complex64 = ieee_value(0., ieee_quiet_nan)
-nan_complex128 = ieee_value(0.,ieee_quiet_nan)
-nan_complex256 = ieee_value(0.,ieee_quiet_nan)
-
-print fmt,'real',storage_size(nan_real128),nan_real128, ieee_is_nan(nan_real128), nan_real128
-
-print fmt,'complex',storage_size(nan_complex64),real(nan_complex64),ieee_is_nan(real(nan_complex64)),nan_complex64
-print fmt,'complex',storage_size(nan_complex128),real(nan_complex128),ieee_is_nan(real(nan_complex128)),nan_complex128
-print fmt,'complex',storage_size(nan_complex256),real(nan_complex256),ieee_is_nan(real(nan_complex256)),nan_complex256
-
-end block
-#endif
 
 end program
---
+++ src/real/nans_128.f90
@@ -0,0 +1,27 @@
+program nans
+
+use, intrinsic :: iso_fortran_env, only : real128, real64, real32
+use, intrinsic :: ieee_arithmetic, only: ieee_value, ieee_quiet_nan, ieee_is_nan
+
+implicit none
+
+character(*), parameter :: fmt='(A8,I3,2X,F5.1,6X,L1,2X,Z32)'
+
+real(real128) :: nan_real128
+
+complex(real32) :: nan_complex64
+complex(real64) :: nan_complex128
+complex(real128) :: nan_complex256
+
+nan_real128 = ieee_value(0.,ieee_quiet_nan)
+nan_complex64 = ieee_value(0., ieee_quiet_nan)
+nan_complex128 = ieee_value(0.,ieee_quiet_nan)
+nan_complex256 = ieee_value(0.,ieee_quiet_nan)
+
+print fmt,'real',storage_size(nan_real128),nan_real128, ieee_is_nan(nan_real128), nan_real128
+
+print fmt,'complex',storage_size(nan_complex64),real(nan_complex64),ieee_is_nan(real(nan_complex64)),nan_complex64
+print fmt,'complex',storage_size(nan_complex128),real(nan_complex128),ieee_is_nan(real(nan_complex128)),nan_complex128
+print fmt,'complex',storage_size(nan_complex256),real(nan_complex256),ieee_is_nan(real(nan_complex256)),nan_complex256
+
+end program
