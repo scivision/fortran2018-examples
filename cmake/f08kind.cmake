6cec8d0198dd7bc6660bdedb425525d4efc0897a
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-07 00:49:32 -0400

    enhance f08kind test with ieee_is_nan

--- cmake/f08kind.cmake
+++ cmake/f08kind.cmake
@@ -1,8 +1,10 @@
 check_source_compiles(Fortran
 "program test
 use, intrinsic:: iso_fortran_env, only: real128
+use, intrinsic:: ieee_arithmetic, only: ieee_is_nan
 
 print *, huge(0._real128)
+print *, ieee_is_nan(0._real128)
 
 end program"
 f08kind)
