4b8f0f6c2c7f8191bde0e434ea35b27f02f1115c
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-07 00:39:31 -0400

    don't use fortran 2003 intrinsic initialization for AOCC compiler

--- src/real/array.f90
+++ src/real/array.f90
@@ -5,9 +5,12 @@ use, intrinsic :: iso_fortran_env, only : real32, real64
 
 implicit none
 
-real(real64), parameter :: s64 = sum([real(real64) :: 0.1, 0.2])
-real(real64), parameter :: s6432 = sum([real(real32) :: 0.1, 0.2])
-real(real32), parameter :: s32 = sum([real(real32) :: 0.1, 0.2])
+real(real64) :: s64, s6432
+real(real32) :: s32
+
+s64 = sum([real(real64) :: 0.1, 0.2])
+s6432 = sum([real(real32) :: 0.1, 0.2])
+s32 = sum([real(real32) :: 0.1, 0.2])
 
 if (s64 == s32) error stop "sum(real64) should not equal sum(real32) in general"
 print *, s6432 == s32  !< often true but maybe not always?
