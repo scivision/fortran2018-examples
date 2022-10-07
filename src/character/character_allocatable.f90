7db17dfa09ada865957b80a3bd8fc172d36e612d
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-07 01:05:05 -0400

    allocate character: add comparison test

--- src/character/character_allocatable.f90
+++ src/character/character_allocatable.f90
@@ -48,8 +48,11 @@ deallocate(flex)
 allocate(character(3) :: flex(2))
 flex = [character(3) :: 'hi', 'bye']
 if (size(flex) /= 2) error stop
-if (len(flex(1)) /= 3) error stop
-if (len_trim(flex(1)) /= 2) error stop
+if (len(flex(1)) /= 3) error stop "flex: len(flex(1)) /= 3"
+if (len_trim(flex(1)) /= 2) error stop "flex: len_trim(flex(1)) /= 2"
+if (flex(1) /= 'hi') error stop "flex: flex(1) /= 'hi'"
+if (flex(2) /= 'bye') error stop "flex: flex(2) /= 'bye'"
+
 
 print *, "OK: allocatable character"
 
