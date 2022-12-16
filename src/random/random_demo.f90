24ca5b5bc4a4ba097e197d5205d9ae2aa2090d81
Michael Hirsch <scivision@users.noreply.github.com>
2022-12-16 14:07:00 -0500

    random_init: print results

--- src/random/random_demo.f90
+++ src/random/random_demo.f90
@@ -1,15 +1,19 @@
 program demo_rand
 
+use, intrinsic :: iso_fortran_env, only : stderr=>error_unit
 implicit none
 
 real :: r1, r2
-
-print *, 'these two random number should not match if random_init is working'
 
 call random_init(.false., .false.)
 call random_number(r1)
 call random_init(.false., .false.)
 call random_number(r2)
-if (r1==r2) error stop 'random_init fail'
+if (r1==r2) then
+  write(stderr,*) 'random_init fail: ', r1, ' == ', r2
+  error stop 'these two random number should not match if random_init is working'
+endif
+
+print *, "OK: random_init: ", r1, " /= ", r2
 
 end program
