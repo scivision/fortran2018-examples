50e081bd8f3588b37de4fa22f5196273507c11e0
scivision <scivision@users.noreply.github.com>
2024-05-21 14:49:54 -0400

    random_init: improved check'

--- cmake/f18random.cmake
+++ cmake/f18random.cmake
@@ -4,3 +4,11 @@ call random_init(.false., .false.)
 end program"
 f18random
 )
+
+if(NOT f18random)
+  return()
+endif()
+
+file(READ ${PROJECT_SOURCE_DIR}/test/standard/random_init.f90 _s)
+
+check_source_runs(Fortran ${_s} f18random_init)
--- test/standard/random_init.f90
+++ test/standard/random_init.f90
@@ -1,7 +1,7 @@
 program rand_init_seed
 !! by Michael Hirsch, enhanced by Steve Lionel to better match Fortran Standard specification.
 
-use, intrinsic :: iso_fortran_env, only : compiler_version
+use, intrinsic :: iso_fortran_env, only : compiler_version, stderr=>error_unit
 implicit none
 
 integer :: s
@@ -10,17 +10,21 @@ integer, allocatable :: seed1(:),seed2(:)
 print *, compiler_version()
 
 call random_seed(size=s)
+print '(a,i0)', "random_seed size: ", s
 allocate (seed1(s),seed2(s))
 
 call random_init(.false., .false.)
 call random_seed (get=seed1)
-print *, "Seed 1:", seed1
+print *, "Seed 1: ", seed1
 
 call random_init(.false., .false.)
 call random_seed(get=seed2)
-print *, "Seed 2:", seed2
+print *, "Seed 2: ", seed2
 
-if (all(seed1==seed2)) error stop 'random_init fail: these two seeds should not match if random_init is working'
+if (all(seed1==seed2)) then
+    write(stderr, '(a)') 'ERROR: random_init(.false., .false.): two seeds above should not match if random_init is working'
+    error stop
+endif
 
 print *, "OK: random_init"
 
