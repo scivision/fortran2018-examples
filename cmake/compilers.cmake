670f2afe5d627ca3437c36cbe786c038ee6e7bef
scivision <scivision@users.noreply.github.com>
2026-01-13 20:58:37 -0500

    correct f2023 rank() test

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,6 +1,6 @@
 # -- compiler feature checks BEFORE setting flags to avoid intermittant failures in general
 
-if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+if(CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM")
   add_compile_options(-traceback -heap-arrays
   "$<$<CONFIG:Debug,RelWithDebInfo>:SHELL:-warn all;SHELL:-debug all;SHELL:-check all>"
   )
@@ -13,9 +13,4 @@ elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   )
 
 #   "$<$<COMPILE_LANGAUGE:Fortran>:-Wrealloc-lhs>"  # not -Wrealloc-lhs-all which warns on character
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "NAG")
-  # https://support.nag.com/nagware/np/r71_doc/manual/compiler_2_4.html#OPTIONS
-  add_compile_options(
-  "$<$<COMPILE_LANGUAGE:Fortran>:-f2018;-C;-colour;-gline;-nan;-info;-u>"
-  )
 endif()
--- cmake/f2023rank_integer.cmake
+++ cmake/f2023rank_integer.cmake
@@ -3,8 +3,11 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 # https://wg5-fortran.org/N2201-N2250/N2212.pdf
 # section 8.3 Using an integer constant to specify rank
 
-check_source_compiles(Fortran "program test
+# Supported by Intel oneAPI >= 2025.3.0
+# not yet supporte by GCC 15.2.0 ...
+
+check_source_compiles(Fortran "subroutine r(a)
 real, rank(2) :: a
 !! Equivalent to real :: a(:,:)
-end program"
+end subroutine"
 f2023rank_integer)
