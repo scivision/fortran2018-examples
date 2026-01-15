d03cd199a2fd25ed089e53d179bed89c0bdb83a8
scivision <scivision@users.noreply.github.com>
2026-01-15 17:26:14 -0500

    f03 select type check more realistic

--- cmake/f03selectType.cmake
+++ cmake/f03selectType.cmake
@@ -3,14 +3,19 @@ block()
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 check_source_compiles(Fortran "subroutine selector(x)
+use, intrinsic :: iso_fortran_env
 
 class(*), intent(in) :: x
 
 select type (x)
-  type is (real)
-    print '(a)', 'real'
-  type is (integer)
-    print '(a)', 'integer'
+  type is (real(real32))
+    print '(a)', 'real32'
+  type is (real(real64))
+    print '(a)', 'real64'
+  type is (integer(int32))
+    print '(a)', 'int32'
+  class default
+    print '(a)', 'unknown type'
 end select
 
 end subroutine"
